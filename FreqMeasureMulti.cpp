7e17d1092dfd3cf02345bc768be17798872b79f9
PaulStoffregen <paul@pjrc.com>
2017-08-11 03:56:01 -0700

    Improve countToNanoseconds

--- FreqMeasureMulti.cpp
+++ FreqMeasureMulti.cpp
@@ -146,9 +146,9 @@ float FreqMeasureMulti::countToFrequency(uint32_t count)
 float FreqMeasureMulti::countToNanoseconds(uint32_t count)
 {
 #if defined(__arm__) && defined(TEENSYDUINO) && defined(KINETISK)
-	return (float)(count * 1000) / (float)(F_BUS / 1000000);
+	return (float)count * (1000000000.0f / (float)F_BUS);
 #elif defined(__arm__) && defined(TEENSYDUINO) && defined(KINETISL)
-	return (float)(count * 1000) / (float)(F_PLL / 2000000);
+	return (float)count * (2000000000.0f / (float)F_PLL);
 #else
 	return 0.0;
 #endif
